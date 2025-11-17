ceb5dd08d061b87979465411a64041efd1762f5f
luocl <2301155695@qq.com>
2025-11-17 10:52:15 +0800

    用缓存空对象的方案，解决缓存穿透；但是我们用的是hash结构，和视频里面的不一样，hash结构不允许放一个 new HashMap<>()进去，所以我们加了一个字段empty:1代表是缓存的空对象；对应黑马点评课程18节

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -6,6 +6,7 @@ import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -27,6 +27,7 @@ import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
@@ -40,18 +41,35 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Override
     public Result queryShopById(Long id) {
 //        1.从redis查询商铺缓存的商铺信息
-        Map<Object, Object> shopMap = stringRedisTemplate.opsForHash().entries(CACHE_SHOP_KEY + id);
+        String cacheKey = CACHE_SHOP_KEY + id;
         Shop shop = new Shop();
-        if (!shopMap.isEmpty()) {
-//            2.如果有缓存的商铺信息，则转map类型为Shop类型后返回，用到hutool工具的fillBeanWithMap
+        boolean keyExists = stringRedisTemplate.hasKey(cacheKey);
+        if (keyExists) {  //用缓存空对象的方式，解决缓存穿透
+            Map<Object, Object> shopMap = stringRedisTemplate.opsForHash().entries(cacheKey);
+            // 2. 判断是否是空对象缓存（仅包含占位字段）
+            if (shopMap.size() == 1 && "empty".equals(shopMap.keySet().iterator().next())) {
+                log.info("命中空对象缓存，直接返回：{}", cacheKey);
+                return Result.fail("店铺不存在");
+            }
+            // 3. 是正常缓存数据，转Bean返回
             shop = BeanUtil.fillBeanWithMap(shopMap, new Shop(), false);
+            log.info("命中正常店铺缓存，返回数据：{}", shop.getId());
             return Result.ok(shop);
         }
 //        3.redis没有缓存的话，去查数据库,用到mybatis-plus的写法
+        log.info("Redis无缓存，查询数据库，店铺ID：{}", id);
         shop = getById(id);
 //        4.判断数据库中能否正确查出来
-        if (shop == null) return Result.fail("店铺不存在");
-        else {  //能查出来就，把信息放入redis并返回前端
+        if (shop == null) {
+            //用缓存空对象的方式，解决缓存穿透,放入一个空的map对象
+            Map<Object, Object> nullCacheMap = new HashMap<>();
+            nullCacheMap.put("empty", "1"); // 关键：添加占位字段
+            stringRedisTemplate.opsForHash().putAll(cacheKey, nullCacheMap);
+            stringRedisTemplate.expire(cacheKey, CACHE_NULL_TTL, TimeUnit.MINUTES); // 空对象缓存时间更短
+            log.info("数据库无此店铺，写入空对象缓存（带占位字段）：{}", cacheKey);
+            return Result.fail("店铺不存在");
+        } else {  //能查出来就，把信息放入redis并返回前端
+            Map<Object, Object> shopMap = new HashMap<>();
             shopMap = new HashMap<>();
             shopMap.put("id", shop.getId().toString());
             shopMap.put("name", shop.getName());
@@ -68,8 +86,8 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             shopMap.put("openHours", shop.getOpenHours().toString());
             shopMap.put("createTime", shop.getCreateTime().toString());
             shopMap.put("updateTime", shop.getUpdateTime().toString());
-            stringRedisTemplate.opsForHash().putAll(CACHE_SHOP_KEY + id, shopMap);
-            stringRedisTemplate.expire(CACHE_SHOP_KEY + id, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+            stringRedisTemplate.opsForHash().putAll(cacheKey, shopMap);
+            stringRedisTemplate.expire(cacheKey, CACHE_SHOP_TTL, TimeUnit.MINUTES);
             return Result.ok(shop);
         }
     }
